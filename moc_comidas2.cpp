4931140e0dc3f53527126dfbf639f18ee974ee62
vladcsw <67337789+vladcsw@users.noreply.github.com>
2020-08-07 17:45:42 -0500

    Add files via upload

---
+++ moc_comidas2.cpp
@@ -0,0 +1,202 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'comidas2.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../paraps/comidas2.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'comidas2.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.15.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_comidas2_t {
+    QByteArrayData data[21];
+    char stringdata0[462];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_comidas2_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_comidas2_t qt_meta_stringdata_comidas2 = {
+    {
+QT_MOC_LITERAL(0, 0, 8), // "comidas2"
+QT_MOC_LITERAL(1, 9, 21), // "on_pushButton_clicked"
+QT_MOC_LITERAL(2, 31, 0), // ""
+QT_MOC_LITERAL(3, 32, 23), // "on_pushButton_2_clicked"
+QT_MOC_LITERAL(4, 56, 21), // "on_comboBox_activated"
+QT_MOC_LITERAL(5, 78, 5), // "index"
+QT_MOC_LITERAL(6, 84, 23), // "on_comboBox_2_activated"
+QT_MOC_LITERAL(7, 108, 23), // "on_comboBox_4_activated"
+QT_MOC_LITERAL(8, 132, 23), // "on_comboBox_5_activated"
+QT_MOC_LITERAL(9, 156, 24), // "on_comboBox_p2_activated"
+QT_MOC_LITERAL(10, 181, 25), // "on_comboBox_p2c_activated"
+QT_MOC_LITERAL(11, 207, 24), // "on_comboBox_p3_activated"
+QT_MOC_LITERAL(12, 232, 25), // "on_comboBox_p3c_activated"
+QT_MOC_LITERAL(13, 258, 24), // "on_comboBox_p4_activated"
+QT_MOC_LITERAL(14, 283, 25), // "on_comboBox_p4c_activated"
+QT_MOC_LITERAL(15, 309, 24), // "on_comboBox_b2_activated"
+QT_MOC_LITERAL(16, 334, 25), // "on_comboBox_b2c_activated"
+QT_MOC_LITERAL(17, 360, 24), // "on_comboBox_b3_activated"
+QT_MOC_LITERAL(18, 385, 25), // "on_comboBox_b3c_activated"
+QT_MOC_LITERAL(19, 411, 24), // "on_comboBox_b4_activated"
+QT_MOC_LITERAL(20, 436, 25) // "on_comboBox_b4c_activated"
+
+    },
+    "comidas2\0on_pushButton_clicked\0\0"
+    "on_pushButton_2_clicked\0on_comboBox_activated\0"
+    "index\0on_comboBox_2_activated\0"
+    "on_comboBox_4_activated\0on_comboBox_5_activated\0"
+    "on_comboBox_p2_activated\0"
+    "on_comboBox_p2c_activated\0"
+    "on_comboBox_p3_activated\0"
+    "on_comboBox_p3c_activated\0"
+    "on_comboBox_p4_activated\0"
+    "on_comboBox_p4c_activated\0"
+    "on_comboBox_b2_activated\0"
+    "on_comboBox_b2c_activated\0"
+    "on_comboBox_b3_activated\0"
+    "on_comboBox_b3c_activated\0"
+    "on_comboBox_b4_activated\0"
+    "on_comboBox_b4c_activated"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_comidas2[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      18,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  104,    2, 0x08 /* Private */,
+       3,    0,  105,    2, 0x08 /* Private */,
+       4,    1,  106,    2, 0x08 /* Private */,
+       6,    1,  109,    2, 0x08 /* Private */,
+       7,    1,  112,    2, 0x08 /* Private */,
+       8,    1,  115,    2, 0x08 /* Private */,
+       9,    1,  118,    2, 0x08 /* Private */,
+      10,    1,  121,    2, 0x08 /* Private */,
+      11,    1,  124,    2, 0x08 /* Private */,
+      12,    1,  127,    2, 0x08 /* Private */,
+      13,    1,  130,    2, 0x08 /* Private */,
+      14,    1,  133,    2, 0x08 /* Private */,
+      15,    1,  136,    2, 0x08 /* Private */,
+      16,    1,  139,    2, 0x08 /* Private */,
+      17,    1,  142,    2, 0x08 /* Private */,
+      18,    1,  145,    2, 0x08 /* Private */,
+      19,    1,  148,    2, 0x08 /* Private */,
+      20,    1,  151,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    5,
+    QMetaType::Void, QMetaType::Int,    5,
+    QMetaType::Void, QMetaType::Int,    5,
+    QMetaType::Void, QMetaType::Int,    5,
+    QMetaType::Void, QMetaType::Int,    5,
+    QMetaType::Void, QMetaType::Int,    5,
+    QMetaType::Void, QMetaType::Int,    5,
+    QMetaType::Void, QMetaType::Int,    5,
+    QMetaType::Void, QMetaType::Int,    5,
+    QMetaType::Void, QMetaType::Int,    5,
+    QMetaType::Void, QMetaType::Int,    5,
+    QMetaType::Void, QMetaType::Int,    5,
+    QMetaType::Void, QMetaType::Int,    5,
+    QMetaType::Void, QMetaType::Int,    5,
+    QMetaType::Void, QMetaType::Int,    5,
+    QMetaType::Void, QMetaType::Int,    5,
+
+       0        // eod
+};
+
+void comidas2::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<comidas2 *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_pushButton_clicked(); break;
+        case 1: _t->on_pushButton_2_clicked(); break;
+        case 2: _t->on_comboBox_activated((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 3: _t->on_comboBox_2_activated((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 4: _t->on_comboBox_4_activated((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 5: _t->on_comboBox_5_activated((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 6: _t->on_comboBox_p2_activated((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 7: _t->on_comboBox_p2c_activated((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 8: _t->on_comboBox_p3_activated((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 9: _t->on_comboBox_p3c_activated((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 10: _t->on_comboBox_p4_activated((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 11: _t->on_comboBox_p4c_activated((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 12: _t->on_comboBox_b2_activated((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 13: _t->on_comboBox_b2c_activated((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 14: _t->on_comboBox_b3_activated((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 15: _t->on_comboBox_b3c_activated((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 16: _t->on_comboBox_b4_activated((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 17: _t->on_comboBox_b4c_activated((*reinterpret_cast< int(*)>(_a[1]))); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject comidas2::staticMetaObject = { {
+    QMetaObject::SuperData::link<QWidget::staticMetaObject>(),
+    qt_meta_stringdata_comidas2.data,
+    qt_meta_data_comidas2,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *comidas2::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *comidas2::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_comidas2.stringdata0))
+        return static_cast<void*>(this);
+    return QWidget::qt_metacast(_clname);
+}
+
+int comidas2::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 18)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 18;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 18)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 18;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
